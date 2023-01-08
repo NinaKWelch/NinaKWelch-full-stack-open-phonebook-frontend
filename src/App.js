5abc0c7ed7dc78d7efd2f2cf0c05a823b0b80567
NinaWelch <nwelch1@live.co.uk>
2023-01-08 14:40:50 +0000

    Fetchdata form dev server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import personService from './services/persons'
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState(null)
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(initialPersons => setPersons(initialPersons))
+  }, [])
+
+  console.log(persons)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Phonebook</h1>
     </div>
   );
 }
 
-export default App;
+export default App
